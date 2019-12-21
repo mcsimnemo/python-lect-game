d5481d21648236ccff8fa180927ab4efb4a5826c
mcsimnemo <saveliev.maxim@gmail.com>
2019-12-21 13:31:36 +0200

    Updated class Nature extra constructor was added

--- nature.py
+++ nature.py
@@ -6,6 +6,15 @@ class Nature(object):
         self.position_y = y
         self.resource = 1000  ## in futute make it randomef
 
+    def __init__(self, nature_type, x, y, res):
+        self.type = nature_type
+        self.position_x = x
+        self.position_y = y
+        self.resource = res
+
+    def set_resource(self, res):
+        self.resource = res
+
     def get_resource(self):
         self.resource -= 10
 
